303d49efc54a40e7642c6db9f007b90536fc7ca2
daisuke-hara0515 <wildlife402@gmail.com>
2021-02-28 19:11:24 +0900

    認証に関するコード追記

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     if Rails.env.development? || Rails.env.test?
-      provider :github, "YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET" 
+      provider :github, "f922eb0448d15a80257a", "b10802d00fc063ab4a81501fd4b4e8a0746d994c" 
     else
       provider :github,
         Rails.application.credentials.github[:client_id],
