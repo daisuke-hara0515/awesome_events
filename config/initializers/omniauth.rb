4aacabcbe493badf0ba03bb419f04c8ff62bb012
daisuke-hara0515 <wildlife402@gmail.com>
2021-02-28 15:45:45 +0900

    omniauth設定

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+Rails.application.config.middleware.use
+OminiAuth::Builder do
+    if Rails.env.development? || Rails.env.test?
+        provider :github, "YOUR_CLIENT_ID","YOUR_CLIENT_SECRET"
+    else
+        provider :github,
+            Rails.application.credentials.github[:client_id],
+            Rails.application.credentials.github[:client_secret]
+    end
+end
