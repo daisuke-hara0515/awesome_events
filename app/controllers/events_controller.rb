9f2d60236c40960a395f5e34836e6587bfd46f7d
daisuke-hara0515 <wildlife402@gmail.com>
2021-03-03 23:52:33 +0900

    destroyメソッドを追記

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,12 @@ class EventsController < ApplicationController
         end
     end
 
+    def destroy
+        @event = current_user.created_events.find(params[:id])
+        @event.destroy!
+        redirect_to root_path, notice: "削除しました"
+    end
+
     def show
         @event = Event.find(params[:id])
     end
