53925da6f89fdf8cd5cf71643f8efe861aa1ee28
daisuke-hara0515 <wildlife402@gmail.com>
2021-03-04 09:14:01 +0900

    イベント参加用のルーティング設定(Ticketとeventを親子関係にする？)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :events do
+    resources :tickets
+  end
 end
